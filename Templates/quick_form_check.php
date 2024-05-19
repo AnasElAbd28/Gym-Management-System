6a235d536dd3f4f412796addc82ab508177eca3c
AnasElAbd28 <130975827+AnasElAbd28@users.noreply.github.com>
2024-05-19 08:20:10 +0800

    forth burger

--- Templates/quick_form_check.php
+++ Templates/quick_form_check.php
@@ -48,6 +48,11 @@
         <li><a href="#">Link 1</a></li>
         <li><a href="#">Link 2</a></li>
         <li><a href="#">Link 3</a></li>
+        <li><a href="#">Link 1</a></li>
+        <li><a href="#">Link 2</a></li>
+        <li><a href="#">Link 3</a></li>
+        <li><a href="#">Link 3</a></li>
+        
     </ul>
     </div>
     <div class="burger">
